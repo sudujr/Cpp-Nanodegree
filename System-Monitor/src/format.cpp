8f841d02343dc5d70edb6fe716a46848fdd29026
Sudharshan <sjsudharshan16@gmail.com>
2020-04-26 13:01:47 +0530

    Add files via upload

---
+++ System-Monitor/src/format.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+#include "format.h"
+
+using std::string;
+using std::to_string;
+// TODO: Complete this helper function
+// INPUT: Long int measuring seconds
+// OUTPUT: HH:MM:SS
+// REMOVE: [[maybe_unused]] once you define the function
+string Format::ElapsedTime(long seconds) {
+     int hours = (seconds / 3600);
+     int lessthanhr = seconds % 3600;
+     string sign = ":";
+     int minutes = (lessthanhr/60);
+     int second = (lessthanhr % 60);
+     string output = to_string(hours) + sign + to_string(minutes) + sign + to_string(second);
+     return output; 
+     
+     }
